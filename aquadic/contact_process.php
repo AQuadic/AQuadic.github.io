b054afbd2519c3918b55d0b4d971e827e7d6d523
Ahmed Ragab <aragab@pickdevs.co.uk>
2020-10-05 13:08:03 +0200

    update name contact us

--- aquadic/contact_process.php
+++ aquadic/contact_process.php
@@ -13,7 +13,7 @@
 	$headers .= "MIME-Version: 1.0\r\n";
 	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-    $subject = "You have a message from your Saasland.";
+    $subject = "You have a message from your AQuadic.";
 
     $logo = '';
     $link = '#';
