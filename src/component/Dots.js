496e84089c4bf20410bb876bfdb6aa8c98b4d15b
ibrahem hassan <hibrahem266@gmail.com>
2023-01-09 17:32:45 +0200

    dots

--- src/component/Dots.js
+++ src/component/Dots.js
@@ -2,231 +2,231 @@ import React from 'react'
 
 function Dots() {
   return (
-    <div class="bg">
-    <div class="dotWrapper dotWrapper-1">
-      <div class="dot dot-1"></div>
+    <div className="bg">
+    <div className="dotWrapper dotWrapper-1">
+      <div className="dot dot-1"></div>
     </div>
-    <div class="dotWrapper dotWrapper-2">
-      <div class="dot dot-2"></div>
+    <div className="dotWrapper dotWrapper-2">
+      <div className="dot dot-2"></div>
     </div>
-    <div class="dotWrapper dotWrapper-3">
-      <div class="dot dot-3"></div>
+    <div className="dotWrapper dotWrapper-3">
+      <div className="dot dot-3"></div>
     </div>
-    <div class="dotWrapper dotWrapper-4">
-      <div class="dot dot-4"></div>
+    <div className="dotWrapper dotWrapper-4">
+      <div className="dot dot-4"></div>
     </div>
-    <div class="dotWrapper dotWrapper-5">
-      <div class="dot dot-5"></div>
+    <div className="dotWrapper dotWrapper-5">
+      <div className="dot dot-5"></div>
     </div>
-    <div class="dotWrapper dotWrapper-6">
-      <div class="dot dot-6"></div>
+    <div className="dotWrapper dotWrapper-6">
+      <div className="dot dot-6"></div>
     </div>
-    <div class="dotWrapper dotWrapper-7">
-      <div class="dot dot-7"></div>
+    <div className="dotWrapper dotWrapper-7">
+      <div className="dot dot-7"></div>
     </div>
-    <div class="dotWrapper dotWrapper-8">
-      <div class="dot dot-8"></div>
+    <div className="dotWrapper dotWrapper-8">
+      <div className="dot dot-8"></div>
     </div>
-    <div class="dotWrapper dotWrapper-9">
-      <div class="dot dot-9"></div>
+    <div className="dotWrapper dotWrapper-9">
+      <div className="dot dot-9"></div>
     </div>
-    <div class="dotWrapper dotWrapper-10">
-      <div class="dot dot-10"></div>
+    <div className="dotWrapper dotWrapper-10">
+      <div className="dot dot-10"></div>
     </div>
-    <div class="dotWrapper dotWrapper-11">
-      <div class="dot dot-11"></div>
+    <div className="dotWrapper dotWrapper-11">
+      <div className="dot dot-11"></div>
     </div>
-    <div class="dotWrapper dotWrapper-12">
-      <div class="dot dot-12"></div>
+    <div className="dotWrapper dotWrapper-12">
+      <div className="dot dot-12"></div>
     </div>
-    <div class="dotWrapper dotWrapper-13">
-      <div class="dot dot-13"></div>
+    <div className="dotWrapper dotWrapper-13">
+      <div className="dot dot-13"></div>
     </div>
-    <div class="dotWrapper dotWrapper-14">
-      <div class="dot dot-14"></div>
+    <div className="dotWrapper dotWrapper-14">
+      <div className="dot dot-14"></div>
     </div>
-    <div class="dotWrapper dotWrapper-15">
-      <div class="dot dot-15"></div>
+    <div className="dotWrapper dotWrapper-15">
+      <div className="dot dot-15"></div>
     </div>
-    <div class="dotWrapper dotWrapper-16">
-      <div class="dot dot-16"></div>
+    <div className="dotWrapper dotWrapper-16">
+      <div className="dot dot-16"></div>
     </div>
-    <div class="dotWrapper dotWrapper-17">
-      <div class="dot dot-17"></div>
+    <div className="dotWrapper dotWrapper-17">
+      <div className="dot dot-17"></div>
     </div>
-    <div class="dotWrapper dotWrapper-18">
-      <div class="dot dot-18"></div>
+    <div className="dotWrapper dotWrapper-18">
+      <div className="dot dot-18"></div>
     </div>
-    <div class="dotWrapper dotWrapper-19">
-      <div class="dot dot-19"></div>
+    <div className="dotWrapper dotWrapper-19">
+      <div className="dot dot-19"></div>
     </div>
-    <div class="dotWrapper dotWrapper-20">
-      <div class="dot dot-20"></div>
+    <div className="dotWrapper dotWrapper-20">
+      <div className="dot dot-20"></div>
     </div>
-    <div class="dotWrapper dotWrapper-21">
-      <div class="dot dot-21"></div>
+    <div className="dotWrapper dotWrapper-21">
+      <div className="dot dot-21"></div>
     </div>
-    <div class="dotWrapper dotWrapper-22">
-      <div class="dot dot-22"></div>
+    <div className="dotWrapper dotWrapper-22">
+      <div className="dot dot-22"></div>
     </div>
-    <div class="dotWrapper dotWrapper-23">
-      <div class="dot dot-23"></div>
+    <div className="dotWrapper dotWrapper-23">
+      <div className="dot dot-23"></div>
     </div>
-    <div class="dotWrapper dotWrapper-24">
-      <div class="dot dot-24"></div>
+    <div className="dotWrapper dotWrapper-24">
+      <div className="dot dot-24"></div>
     </div>
-    <div class="dotWrapper dotWrapper-25">
-      <div class="dot dot-25"></div>
+    <div className="dotWrapper dotWrapper-25">
+      <div className="dot dot-25"></div>
     </div>
-    <div class="dotWrapper dotWrapper-26">
-      <div class="dot dot-26"></div>
+    <div className="dotWrapper dotWrapper-26">
+      <div className="dot dot-26"></div>
     </div>
-    <div class="dotWrapper dotWrapper-27">
-      <div class="dot dot-27"></div>
+    <div className="dotWrapper dotWrapper-27">
+      <div className="dot dot-27"></div>
     </div>
-    <div class="dotWrapper dotWrapper-28">
-      <div class="dot dot-28"></div>
+    <div className="dotWrapper dotWrapper-28">
+      <div className="dot dot-28"></div>
     </div>
-    <div class="dotWrapper dotWrapper-29">
-      <div class="dot dot-29"></div>
+    <div className="dotWrapper dotWrapper-29">
+      <div className="dot dot-29"></div>
     </div>
-    <div class="dotWrapper dotWrapper-30">
-      <div class="dot dot-30"></div>
+    <div className="dotWrapper dotWrapper-30">
+      <div className="dot dot-30"></div>
     </div>
-    <div class="dotWrapper dotWrapper-31">
-      <div class="dot dot-31"></div>
+    <div className="dotWrapper dotWrapper-31">
+      <div className="dot dot-31"></div>
     </div>
-    <div class="dotWrapper dotWrapper-32">
-      <div class="dot dot-32"></div>
+    <div className="dotWrapper dotWrapper-32">
+      <div className="dot dot-32"></div>
     </div>
-    <div class="dotWrapper dotWrapper-33">
-      <div class="dot dot-33"></div>
+    <div className="dotWrapper dotWrapper-33">
+      <div className="dot dot-33"></div>
     </div>
-    <div class="dotWrapper dotWrapper-34">
-      <div class="dot dot-34"></div>
+    <div className="dotWrapper dotWrapper-34">
+      <div className="dot dot-34"></div>
     </div>
-    <div class="dotWrapper dotWrapper-35">
-      <div class="dot dot-35"></div>
+    <div className="dotWrapper dotWrapper-35">
+      <div className="dot dot-35"></div>
     </div>
-    <div class="dotWrapper dotWrapper-36">
-      <div class="dot dot-36"></div>
+    <div className="dotWrapper dotWrapper-36">
+      <div className="dot dot-36"></div>
     </div>
-    <div class="dotWrapper dotWrapper-37">
-      <div class="dot dot-37"></div>
+    <div className="dotWrapper dotWrapper-37">
+      <div className="dot dot-37"></div>
     </div>
-    <div class="dotWrapper dotWrapper-38">
-      <div class="dot dot-38"></div>
+    <div className="dotWrapper dotWrapper-38">
+      <div className="dot dot-38"></div>
     </div>
-    <div class="dotWrapper dotWrapper-39">
-      <div class="dot dot-39"></div>
+    <div className="dotWrapper dotWrapper-39">
+      <div className="dot dot-39"></div>
     </div>
-    <div class="dotWrapper dotWrapper-40">
-      <div class="dot dot-40"></div>
+    <div className="dotWrapper dotWrapper-40">
+      <div className="dot dot-40"></div>
     </div>
-    <div class="dotWrapper dotWrapper-41">
-      <div class="dot dot-41"></div>
+    <div className="dotWrapper dotWrapper-41">
+      <div className="dot dot-41"></div>
     </div>
-    <div class="dotWrapper dotWrapper-42">
-      <div class="dot dot-42"></div>
+    <div className="dotWrapper dotWrapper-42">
+      <div className="dot dot-42"></div>
     </div>
-    <div class="dotWrapper dotWrapper-43">
-      <div class="dot dot-43"></div>
+    <div className="dotWrapper dotWrapper-43">
+      <div className="dot dot-43"></div>
     </div>
-    <div class="dotWrapper dotWrapper-44">
-      <div class="dot dot-44"></div>
+    <div className="dotWrapper dotWrapper-44">
+      <div className="dot dot-44"></div>
     </div>
-    <div class="dotWrapper dotWrapper-45">
-      <div class="dot dot-45"></div>
+    <div className="dotWrapper dotWrapper-45">
+      <div className="dot dot-45"></div>
     </div>
-    <div class="dotWrapper dotWrapper-46">
-      <div class="dot dot-46"></div>
+    <div className="dotWrapper dotWrapper-46">
+      <div className="dot dot-46"></div>
     </div>
-    <div class="dotWrapper dotWrapper-47">
-      <div class="dot dot-47"></div>
+    <div className="dotWrapper dotWrapper-47">
+      <div className="dot dot-47"></div>
     </div>
-    <div class="dotWrapper dotWrapper-48">
-      <div class="dot dot-48"></div>
+    <div className="dotWrapper dotWrapper-48">
+      <div className="dot dot-48"></div>
     </div>
-    <div class="dotWrapper dotWrapper-49">
-      <div class="dot dot-49"></div>
+    <div className="dotWrapper dotWrapper-49">
+      <div className="dot dot-49"></div>
     </div>
-    <div class="dotWrapper dotWrapper-50">
-      <div class="dot dot-50"></div>
+    <div className="dotWrapper dotWrapper-50">
+      <div className="dot dot-50"></div>
     </div>
-    <div class="dotWrapper dotWrapper-51">
-      <div class="dot dot-51"></div>
+    <div className="dotWrapper dotWrapper-51">
+      <div className="dot dot-51"></div>
     </div>
-    <div class="dotWrapper dotWrapper-52">
-      <div class="dot dot-52"></div>
+    <div className="dotWrapper dotWrapper-52">
+      <div className="dot dot-52"></div>
     </div>
-    <div class="dotWrapper dotWrapper-53">
-      <div class="dot dot-53"></div>
+    <div className="dotWrapper dotWrapper-53">
+      <div className="dot dot-53"></div>
     </div>
-    <div class="dotWrapper dotWrapper-54">
-      <div class="dot dot-54"></div>
+    <div className="dotWrapper dotWrapper-54">
+      <div className="dot dot-54"></div>
     </div>
-    <div class="dotWrapper dotWrapper-55">
-      <div class="dot dot-55"></div>
+    <div className="dotWrapper dotWrapper-55">
+      <div className="dot dot-55"></div>
     </div>
-    <div class="dotWrapper dotWrapper-56">
-      <div class="dot dot-56"></div>
+    <div className="dotWrapper dotWrapper-56">
+      <div className="dot dot-56"></div>
     </div>
-    <div class="dotWrapper dotWrapper-57">
-      <div class="dot dot-57"></div>
+    <div className="dotWrapper dotWrapper-57">
+      <div className="dot dot-57"></div>
     </div>
-    <div class="dotWrapper dotWrapper-58">
-      <div class="dot dot-58"></div>
+    <div className="dotWrapper dotWrapper-58">
+      <div className="dot dot-58"></div>
     </div>
-    <div class="dotWrapper dotWrapper-59">
-      <div class="dot dot-59"></div>
+    <div className="dotWrapper dotWrapper-59">
+      <div className="dot dot-59"></div>
     </div>
-    <div class="dotWrapper dotWrapper-60">
-      <div class="dot dot-60"></div>
+    <div className="dotWrapper dotWrapper-60">
+      <div className="dot dot-60"></div>
     </div>
-    <div class="dotWrapper dotWrapper-61">
-      <div class="dot dot-61"></div>
+    <div className="dotWrapper dotWrapper-61">
+      <div className="dot dot-61"></div>
     </div>
-    <div class="dotWrapper dotWrapper-62">
-      <div class="dot dot-62"></div>
+    <div className="dotWrapper dotWrapper-62">
+      <div className="dot dot-62"></div>
     </div>
-    <div class="dotWrapper dotWrapper-63">
-      <div class="dot dot-63"></div>
+    <div className="dotWrapper dotWrapper-63">
+      <div className="dot dot-63"></div>
     </div>
-    <div class="dotWrapper dotWrapper-64">
-      <div class="dot dot-64"></div>
+    <div className="dotWrapper dotWrapper-64">
+      <div className="dot dot-64"></div>
     </div>
-    <div class="dotWrapper dotWrapper-65">
-      <div class="dot dot-65"></div>
+    <div className="dotWrapper dotWrapper-65">
+      <div className="dot dot-65"></div>
     </div>
-    <div class="dotWrapper dotWrapper-66">
-      <div class="dot dot-66"></div>
+    <div className="dotWrapper dotWrapper-66">
+      <div className="dot dot-66"></div>
     </div>
-    <div class="dotWrapper dotWrapper-67">
-      <div class="dot dot-67"></div>
+    <div className="dotWrapper dotWrapper-67">
+      <div className="dot dot-67"></div>
     </div>
-    <div class="dotWrapper dotWrapper-68">
-      <div class="dot dot-68"></div>
+    <div className="dotWrapper dotWrapper-68">
+      <div className="dot dot-68"></div>
     </div>
-    <div class="dotWrapper dotWrapper-69">
-      <div class="dot dot-69"></div>
+    <div className="dotWrapper dotWrapper-69">
+      <div className="dot dot-69"></div>
     </div>
-    <div class="dotWrapper dotWrapper-70">
-      <div class="dot dot-70"></div>
+    <div className="dotWrapper dotWrapper-70">
+      <div className="dot dot-70"></div>
     </div>
-    <div class="dotWrapper dotWrapper-71">
-      <div class="dot dot-71"></div>
+    <div className="dotWrapper dotWrapper-71">
+      <div className="dot dot-71"></div>
     </div>
-    <div class="dotWrapper dotWrapper-72">
-      <div class="dot dot-72"></div>
+    <div className="dotWrapper dotWrapper-72">
+      <div className="dot dot-72"></div>
     </div>
-    <div class="dotWrapper dotWrapper-73">
-      <div class="dot dot-73"></div>
+    <div className="dotWrapper dotWrapper-73">
+      <div className="dot dot-73"></div>
     </div>
-    <div class="dotWrapper dotWrapper-74">
-      <div class="dot dot-74"></div>
+    <div className="dotWrapper dotWrapper-74">
+      <div className="dot dot-74"></div>
     </div>
-    <div class="dotWrapper dotWrapper-75">
-      <div class="dot dot-75"></div>
+    <div className="dotWrapper dotWrapper-75">
+      <div className="dot dot-75"></div>
     </div>
   </div>
   )
