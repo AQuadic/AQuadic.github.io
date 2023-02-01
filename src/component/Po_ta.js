c807a308d52e86cb64d7383c9dbc4dd0338aa926
ibrahem hassan <hibrahem266@gmail.com>
2023-02-01 16:19:00 +0200

    projects and icons

--- src/component/Po_ta.js
+++ src/component/Po_ta.js
@@ -2,11 +2,11 @@ import React from 'react'
 import {Outlet} from 'react-router-dom'
 
 function Po_ta() {
-    return (
-        <div>
-            <Outlet/>
-        </div>
-    )
+  return (
+    <div>
+        <Outlet/>
+    </div>
+  )
 }
 
 export default Po_ta
