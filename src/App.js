1bdb1e4d767d2a02156499e4936c02f8c93ede70
ibrahem hassan <hibrahem266@gmail.com>
2023-02-22 16:32:25 +0200

    remove redux

--- src/App.js
+++ src/App.js
@@ -1,77 +1,60 @@
-import './App.css';
+import "./App.css";
 
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import Home from './component/Home';
-import Career from './component/Career';
-import JobDetails from './component/JobDetails';
-import Hosting from './component/Hosting';
-import TimeModel from './component/TimeModel';
-import Team from './component/Team';
-import Persone from './component/Persone';
-import Portfolio from './component/Portfolio';
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./component/Home";
+import Career from "./component/Career";
+import JobDetails from "./component/JobDetails";
+import Hosting from "./component/Hosting";
+import TimeModel from "./component/TimeModel";
+import Team from "./component/Team";
+import Persone from "./component/Persone";
+import Portfolio from "./component/Portfolio";
 
-import data from './data/projects.json'
-import ProjectDetails from './component/ProjectDetails';
+import data from "./data/projects.json";
+import ProjectDetails from "./component/ProjectDetails";
 
-import ScrollToTop from './component/ScrollToTop';
-import {Provider} from 'react-redux';
-import store from './data/dataSlice';
-import PoTa from './component/PoTa';
-import HomeLay from './component/HomeLay';
-import { useEffect } from 'react';
+import ScrollToTop from "./component/ScrollToTop";
+import PoTa from "./component/PoTa";
+import HomeLay from "./component/HomeLay";
+import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
-import JopAbout from './component/JopAbout';
+import JopAbout from "./component/JopAbout";
 
 function App() {
-   
- const { i18n } = useTranslation();
-    useEffect(()=>{
-        i18n.language === "ar"?window.document.dir='rtl':window.document.dir='ltr'
-       },[i18n.language])
-       console.log(window.document.dir)
-       
- 
-    AOS.init();
+  const { i18n } = useTranslation();
+  useEffect(() => {
+    i18n.language === "ar"
+      ? (window.document.dir = "rtl")
+      : (window.document.dir = "ltr");
+  }, [i18n.language]);
 
-    return (
-        <Provider store={store}>
+  AOS.init();
 
-
-            <div className="App">
-
-
-                <BrowserRouter>
-                    <ScrollToTop/>
-
-
-                    <Routes>
-                        <Route path='/' element={<HomeLay/>}>
-                            <Route index element={<Home/>}/>
-                            <Route path='career' element={<Career/>}/>
-                            <Route path='job' element={<JobDetails/>}/>
-                            <Route path='hosting' element={<Hosting/>}/>
-                            <Route path='time_model' element={<TimeModel/>}/>
-                            <Route path='team' element={<Team/>}/>
-                            <Route path='person' element={<Persone/>}/>
-                            <Route path='portfolio' element={<PoTa/>}>
-                                <Route index element={<Portfolio data={data}/>}/>
-                                <Route path=':id' element={<ProjectDetails data={data}/>}/>
-                            </Route>
-                            <Route path='service/:id' element={<JopAbout/>}/>
-                           
-                           
-                        </Route>
-
-                    </Routes>
-
-                </BrowserRouter>
-
-
-            </div>
-        </Provider>
-    );
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<HomeLay />}>
+            <Route index element={<Home />} />
+            <Route path="career" element={<Career />} />
+            <Route path="job" element={<JobDetails />} />
+            <Route path="hosting" element={<Hosting />} />
+            <Route path="time_model" element={<TimeModel />} />
+            <Route path="team" element={<Team />} />
+            <Route path="person" element={<Persone />} />
+            <Route path="portfolio" element={<PoTa />}>
+              <Route index element={<Portfolio data={data} />} />
+              <Route path=":id" element={<ProjectDetails data={data} />} />
+            </Route>
+            <Route path="service/:id" element={<JopAbout />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- src/data/dataSlice.js
+++
@@ -1,6 +0,0 @@
-import {configureStore} from "@reduxjs/toolkit";
-
-import counterreducer from './data';
-
-const store = configureStore({reducer: {dir: counterreducer}})
-export default store;
